9a33b41c2185f23ee46dea5c59345f50ee890dad
alejo <alejandrowagnerbejaranog@gmail.com>
2025-03-11 19:25:07 +0100

    update repo name & latest hash of kernel

--- ti-linux-kernel-variscite/version.sh
+++ ti-linux-kernel-variscite/version.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-export git_repo="https://github.com/varigit/ti-linux-kernel.git"
+#export git_repo="https://github.com/varigit/ti-linux-kernel.git"
+export git_repo="https://github.com/awagnersaci/ti-linux-kernel.git"
